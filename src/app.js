7eb06f36daa476a007208091db6a3d741177405e
pascalmoser <pascalmoser@gmail.com>
2017-03-16 20:07:06 +0100

    basic timer functionality

--- src/app.js
+++ src/app.js
@@ -13,7 +13,92 @@
 
     function AppCtrl ( $scope, $http, $mdToast, $localStorage ) {
 
-        $scope.time = 15*60;
+        $scope.timer = {
+            state: 'pause', // play, pause, timeout
+            breakTime: false
+        };
+
+        $scope.gameSchedule = [
+            { smallblind:10, bigblind:20, playtime:10, breaktime:9 },
+            { smallblind:20, bigblind:40, playtime:8, breaktime:7 },
+            { smallblind:50, bigblind:100, playtime:6, breaktime:5 },
+            { smallblind:100, bigblind:200, playtime:4, breaktime:3 },
+            { smallblind:200, bigblind:400, playtime:2, breaktime:1 }
+        ];
+
+        $scope.game = {
+            playTime: $scope.gameSchedule[0].playtime,
+            breakTime: 0,
+            bigBlind: 0,
+            smallBlind: 0,
+            scheduleIndex: 0
+        };
+
+        $scope.toggleTimer = function(forcePlay) {
+            if($scope.timer.state !== 'play' || forcePlay === true) {
+                $scope.timer.state = 'play';
+                $scope.playTimer();
+            } else {
+                $scope.timer.state = 'pause';
+                $scope.pauseTimer();
+            }
+        };
+
+        $scope.playTimer = function() {
+            if($scope.game.scheduleIndex === 0) {
+                $scope.scheduler();
+            }
+            $scope.$broadcast('timer-start');
+        };
+
+        $scope.pauseTimer = function() {
+            $scope.$broadcast('timer-stop');
+        };
+
+        $scope.scheduler = function() {
+            if($scope.game.breakTime > 0 && !$scope.timer.breaktime) {
+                console.log('break');
+                $scope.breakTime();
+            } else {
+                $scope.timer.breaktime = false;
+                $scope.game.smallBlind = $scope.gameSchedule[$scope.game.scheduleIndex].smallblind;
+                $scope.game.bigBlind = $scope.gameSchedule[$scope.game.scheduleIndex].bigblind;
+                $scope.game.breakTime = $scope.gameSchedule[$scope.game.scheduleIndex].breaktime;
+                $scope.$broadcast('timer-set-countdown-seconds', $scope.gameSchedule[$scope.game.scheduleIndex].playtime);
+                $scope.game.scheduleIndex++;
+                $scope.toggleTimer(true);
+            }
+        };
+
+        $scope.$on('timer-stopped', function (event, data){
+            if(data.seconds === 0) {
+                $scope.timeOut();
+            }
+        });
+
+        $scope.timeOut = function() {
+            console.log('timeout');
+            if($scope.timer.state !== 'timeout') {
+                $scope.timer.state = 'timeout';
+                $scope.$broadcast('timer-set-countdown-seconds', 5);
+                $scope.$broadcast('timer-start');
+            } else {
+                $scope.scheduler();
+            }
+        };
+
+        $scope.breakTime = function() {
+            $scope.$broadcast('timer-set-countdown-seconds', $scope.game.breakTime);
+            $scope.timer.state = 'pause';
+            $scope.timer.breaktime = true;
+            $scope.$broadcast('timer-start');
+        };
+
+        $scope.pauseTimer = function() {
+            $scope.$broadcast('timer-stop');
+            $scope.timerRunning = false;
+        };
+
 
         $scope.color={
             red: 255,
@@ -70,7 +155,7 @@
                 .action('OK')
                 .highlightAction(true)
                 .textContent(toastMsg)
-                .position('top right')
+                .position('bottom right')
                 .hideDelay(6000)
             );
         };
@@ -105,4 +190,5 @@
             );
         }
     }
+
 })();
